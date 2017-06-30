4f1cb655203b8c73ffcfe170e126f3cd7b706ba4
carlaraya <carl_angelo.araya@upd.edu.ph>
2017-06-30 15:00:04 +0800

    Something that actually works lol

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Make your FB read only!
 
 Removes all like, share and comment boxes.
+
+HOW TO USE: [https://carlaraya.github.io/fbReadOnly/](https://carlaraya.github.io/fbReadOnly/)
--- fbReadOnly.js
+++ fbReadOnly.js
@@ -1,16 +1,15 @@
-classNames = ["UFIAddComment", "UFILikeLink", "share_action_link"];
-idNames = ["timeline_react_composer_container"];
+classNames = ["UFIAddComment", "UFILikeLink", "share_action_link", "PageLikeButton", "groupRHCAddMemberTypeaheadBox"];
+idNames = ["feedx_sprouts_container", "timeline_react_composer_container", "pagelet_group_composer"];
 mainLoop = function() {
-  classNamesToHide.forEach(function(className) {
   for (var i = 0; i < classNames.length; i++) {
-    elems = document.getElementsByClassName(classNames[i]);
+    var elems = document.getElementsByClassName(classNames[i]);
     for (var j = 0; j < elems.length; j++) {
       elems[j].style.display="none";
     }
   }
-  for (var i = 0; i < idNames.length; i++) {
-    elem = document.getElementById(idNames[i]);
-    elem.style.display="none";
+  for (var k = 0; k < idNames.length; k++) {
+    var elem = document.getElementById(idNames[k]);
+    if (elem) elem.style.display="none";
   }
-}
+};
 setInterval(mainLoop, 300);
